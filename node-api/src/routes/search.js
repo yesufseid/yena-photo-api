6e8b781fe616fa02725601407837f0e59039b3d6
Seid Yesuf <seidyesuf750@gmail.com>
2026-06-06 15:42:05 +0300

    >0.5

--- node-api/src/routes/search.js
+++ node-api/src/routes/search.js
@@ -15,26 +15,29 @@ router.post("/", upload.single("image"), async (req, res) => {
     const faceData = await extractFaces(imageBuffer)
       
     if (faceData.faces.length === 0) {
+      console.log("no face found");
+      
       return res.status(400).json({
         error: "No face found",
       });
     }
 
     const embedding = faceData.faces[0].embedding;
+const result = await pool.query(
+  `
+  SELECT
+      photo_id,
+      1 - (embedding <=> $1::vector) AS similarity
+  FROM faces
+  WHERE (1 - (embedding <=> $1::vector)) > 0.5
+  ORDER BY embedding <=> $1::vector
+  LIMIT 20
+  `,
+  [JSON.stringify(embedding)]
+);
 
-    const result = await pool.query(
-      `
-      SELECT
-          photo_id,
-          1 - (embedding <=> $1::vector) as similarity
-      FROM faces
-      ORDER BY embedding <=> $1::vector
-      LIMIT 20
-      `,
-      [JSON.stringify(embedding)]
-    );
-      console.log(result.rows);
-    res.json(result.rows);
+console.log(result.rows);
+res.json(result.rows);
   } catch (error) {
     console.error(error);
     
